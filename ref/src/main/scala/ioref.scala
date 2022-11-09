244310948e055fa15525edbdced55150a4e20155
Zainab Ali <zainabali@C02ZQBZYMD6T.intenthq.com>
2022-11-09 11:37:18 +0000

    Print cat names

--- ref/src/main/scala/ioref.scala
+++ ref/src/main/scala/ioref.scala
@@ -11,6 +11,9 @@ object CatNamesApp extends IOApp.Simple {
   def manyCats: List[String] =
     List("Mao", "Maru", "Popcorn", "Totoro")
 
+  def printAndCount(name: String, counter: Ref[IO, Int]): IO[Unit] =
+    IO.println(s"Counting $name").flatMap(_ => incrementCount(counter))
+
   def incrementCount(counter: Ref[IO, Int]): IO[Unit] =
     counter.get.flatMap(currentvalue => counter.set(currentvalue + 1))
 
@@ -20,7 +23,7 @@ object CatNamesApp extends IOApp.Simple {
   def run: IO[Unit] = {
     val counterRef: IO[Ref[IO, Int]] = Ref.of[IO, Int](0)
     counterRef.flatMap { (counter: Ref[IO, Int]) =>
-      manyCats.parTraverse(_ => incrementCount(counter))
+      manyCats.parTraverse(printAndCount(_, counter))
       .flatMap(_ => printCount(counter))
     }
 
